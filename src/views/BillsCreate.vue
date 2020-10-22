b779a2c59e95383d26c7e04159f4cf20bfd4ae38
Oxman285 <fatman_cah@hotmail.com>
2020-10-22 00:39:28 -0600

    BA-1_Bill-Creation
    updated the json structure, and added initial fields. Is now blocked until BA-4 and BA-6 are completed.

--- src/views/BillsCreate.vue
+++ src/views/BillsCreate.vue
@@ -1,6 +1,24 @@
 <template>
-    <h1>Create a new bill here</h1>
+    <div>
+        <h1>Add Bill</h1>
+        <label for="billName">Name</label>
+        <div v-if="validationFailed('billName')" class="error-detail">{{getErrorMessage('billName')}}</div>
+        <input id="billName" type="text" v-model="bill.name" placeholder="Bill Name" :class="{'error-detail-input': validationFailed('billName')}" />
+        <label for="billAmount">Amount</label>
+        <div v-if="validationFailed('billAmount')" class="error-detail">{{getErrorMessage('billAmount')}}</div>
+        <input id="billAmount" type="text" v-model="bill.amount" placeholder="0.00" :class="{'error-detail-input': validationFailed('billAmount')}" />
+        <label for="billFixedAmount">Is fixed amount?</label>
+        <input id="billFixedAmount" type="checkbox" v-model="bill.isFixedAmount" />
+        <label for="billRecurring">Is recurring?</label>
+        <input id="billRecurring" type="checkbox" v-model="bill.isRecurring" />
+    </div>
 </template>
 <script>
-export default {};
+export default {
+    date() {
+        return {
+            bill: Object
+        }
+    }
+};
 </script>
